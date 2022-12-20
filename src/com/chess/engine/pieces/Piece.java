cc7131f551b4d22001db1843030ccd499f328e08
tinotenda-alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-12-20 09:13:42 +0000

    Add equals() and hashcode() methods for Piece class

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -13,6 +13,7 @@ public abstract class Piece {
     protected final Alliance pieceAlliance;
 
     protected final boolean isFirstMove;
+    private final int cachedHashCode;
 
     Piece(final PieceType pieceType, final int piecePosition, final Alliance pieceAlliance) {
         this.pieceType = pieceType;
@@ -20,6 +21,38 @@ public abstract class Piece {
         this.pieceAlliance = pieceAlliance;
         //TODO somework on this
         this.isFirstMove = false;
+        this.cachedHashCode = computeHashCode();
+    }
+
+    private int computeHashCode() {
+        int result = pieceType.hashCode();
+        result = 31 * result + pieceAlliance.hashCode();
+        result = 31 * result + piecePosition;
+        result = 31 * result + (isFirstMove ? 1 : 0);
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Piece)) {
+            return false;
+        }
+        final Piece otherPiece = (Piece) other;
+        return pieceAlliance == otherPiece.getPieceAlliance() &&
+                pieceType == otherPiece.getPieceType() &&
+                piecePosition == otherPiece.getPiecePosition() &&
+                isFirstMove == otherPiece.isFirstMove();
+
+    }
+
+    @Override
+    public int hashCode() {
+        return this.cachedHashCode;
     }
 
     public int getPiecePosition() {
